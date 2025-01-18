47d28251560ce86b90d41ecced2dff927a5eb8c2
WillYum <liamshere98@gmail.com>
2025-01-19 01:37:49 +0200

    Log draw calls count & setup logic to be able to play animation on spine from console in browser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Spine, DebugUtils, Attachment, VertexAttachment, RegionAttachment, NumberArrayLike, Vector2 } from '@esotericsoftware/spine-pixi-v8';
+import { Spine, RegionAttachment, NumberArrayLike } from '@esotericsoftware/spine-pixi-v8';
 import { Application, Assets } from 'pixi.js';
+
 
 (async () => {
     const app = new Application();
@@ -8,14 +9,42 @@ import { Application, Assets } from 'pixi.js';
 
     document.body.appendChild(app.canvas);
 
+
+    const drawCallsElement = document.getElementById('draw-calls');
+    const vertexCountElement = document.getElementById('vertex-count')!;
+    const trianglesElement = document.getElementById('triangles')!;
+
+
+
+
+    let drawCount = 0;
+
+    const renderer = app.renderer as any;
+    const drawElements = renderer.gl.drawElements;
+    renderer.gl.drawElements = (...args: any[]) => {
+        drawElements.call(renderer.gl, ...args);
+        drawCount++;
+    }; // Rewrite drawElements to count draws
+
+    app.ticker.add(() => {
+        if (drawCallsElement) {
+            drawCallsElement.textContent = drawCount.toString();
+        }
+        drawCount = 0; // Clear count per frame
+    });
+
     const handleOnProgress = (v: any) => {
         console.log('progress', v);
-    }
+    };
 
     Assets.load([
         { alias: 'spineSkeleton', src: './assets_to_test/spineboy-ess.json' },
         { alias: 'spineAtlas', src: './assets_to_test/spineboy.atlas' },
-        { alias: 'spineImage', src: './assets_to_test/spineboy.alias' },
+        { alias: 'spineImage', src: './assets_to_test/spineboy.png' },
+
+        // { alias: 'owl_spineSkeleton', src: './assets_to_test/owl-pro.json' },
+        // { alias: 'owl_spineAtlas', src: './assets_to_test/owl.atlas' },
+        // { alias: 'owl_spineImage', src: './assets_to_test/owl.png' },
 
     ], handleOnProgress).then(() => {
         const spine = Spine.from({
@@ -23,10 +52,26 @@ import { Application, Assets } from 'pixi.js';
             skeleton: "spineSkeleton",
         });
 
-        const verCount = getVertsCount(spine);
-        const triangles = verCount / 2;
 
-        console.log("verCount", verCount, "triangles", triangles);
+        const animName = getAnimationNames(spine);
+        console.log("animName", animName);
+
+
+        window.addEventListener("message", (event) => {
+            const message = event.data;
+            console.log("message", message);
+            if (checkIfAnimationExists(spine, message)) {
+                playAnimation(spine, message);
+            }
+        });
+
+
+        const vertexCount = getVertsCount(spine);
+        const triangles = vertexCount / 2;
+
+        vertexCountElement.textContent = vertexCount.toString();
+        trianglesElement.textContent = triangles.toString();
+
 
         spine.x = app.screen.width / 2;
         spine.y = app.screen.height;
@@ -58,4 +103,17 @@ function getVertsCount(spine: Spine) {
 
 function getVertCounFromUv(uv: NumberArrayLike) {
     return uv.length / 2;
+}
+
+
+function getAnimationNames(spine: Spine) {
+    return spine.skeleton.data.animations.map(anim => anim.name);
+}
+
+function playAnimation(spine: Spine, animName: string) {
+    spine.state.setAnimation(0, animName, true);
+}
+
+function checkIfAnimationExists(spine: Spine, animName: string) {
+    return spine.skeleton.data.animations.some(anim => anim.name === animName);
 }
