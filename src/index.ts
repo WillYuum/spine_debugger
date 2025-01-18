38cdfe7689d4dff5f157d0e54913f437a8e89bea
WillYum <liamshere98@gmail.com>
2025-01-19 00:25:27 +0200

    Implement logic for getting verts and triangle count

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-import { Spine } from '@esotericsoftware/spine-pixi-v8';
+import { Spine, DebugUtils, Attachment, VertexAttachment, RegionAttachment, NumberArrayLike, Vector2 } from '@esotericsoftware/spine-pixi-v8';
 import { Application, Assets } from 'pixi.js';
 
 (async () => {
-    // Create a PixiJS application.
     const app = new Application();
 
-    // Initialize the application.
     await app.init({ background: '#1099bb', resizeTo: window });
 
-    // Add the application's canvas to the DOM body.
     document.body.appendChild(app.canvas);
 
     const handleOnProgress = (v: any) => {
@@ -21,14 +18,15 @@ import { Application, Assets } from 'pixi.js';
         { alias: 'spineImage', src: './assets_to_test/spineboy.alias' },
 
     ], handleOnProgress).then(() => {
-        console.log("Setting up spine");
-
-
-
         const spine = Spine.from({
             atlas: "spineAtlas",
             skeleton: "spineSkeleton",
         });
+
+        const verCount = getVertsCount(spine);
+        const triangles = verCount / 2;
+
+        console.log("verCount", verCount, "triangles", triangles);
 
         spine.x = app.screen.width / 2;
         spine.y = app.screen.height;
@@ -38,3 +36,26 @@ import { Application, Assets } from 'pixi.js';
         console.error("Failed to load assets", error);
     });
 })();
+
+
+function getVertsCount(spine: Spine) {
+    let count = 0;
+    spine.skeleton.slots.forEach(slot => {
+        const attachment = slot.getAttachment();
+
+        if (attachment instanceof RegionAttachment) {
+            const regionAttachment = attachment as RegionAttachment;
+            const vertCount = getVertCounFromUv(regionAttachment.uvs);
+            count += vertCount;
+        } else {
+            console.log("Unknown attachment type");
+        }
+
+    });
+    return count;
+}
+
+
+function getVertCounFromUv(uv: NumberArrayLike) {
+    return uv.length / 2;
+}
