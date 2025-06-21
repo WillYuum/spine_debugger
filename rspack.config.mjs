a632e0a88974588149c688e1c2821c20ba41c4d0
WillYum <liamshere98@gmail.com>
2025-06-22 01:45:32 +0300

    Add support for RSDOCTOR + remove unnecessary from rspack config

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,7 +1,11 @@
 import path from 'node:path';
 import { defineConfig } from '@rspack/cli';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
+import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
+import { rspack } from '@rspack/core';
+
+const shouldAnalyze = process.env.RSDOCTOR === 'true';
+console.log(`Rsdoctor analysis will${shouldAnalyze ? ' 🩺' : ' ❌'} run (process.env.RSDOCTOR=${process.env.RSDOCTOR})`);
 
 export default defineConfig({
     entry: './src/index.ts',
@@ -24,31 +28,27 @@ export default defineConfig({
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
-            {
-                test: /\.(png|json|atlas)$/,
-                type: 'asset/',
-            },
         ],
     },
     resolve: {
         extensions: ['.ts', '.js'],
     },
-    // devServer: {
-    //     static: path.join("./", 'dist'),
-    //     compress: true,
-    //     port: 3000,
-    //     open: true,
-    // },
     plugins: [
         // Automatically generate an index.html file, using the one in the public folder
         new HtmlWebpackPlugin({
             template: './public/index.html', // Path to your HTML template in the public folder
         }),
-        // Copy the index.css from the public folder to the build folder
-        // new CopyPlugin({
-        //     patterns: [
-        //         { from: './public/index.css', to: 'index.css' }, // Path to the CSS file in the public folder
-        //     ],
-        // }),
-    ],
+        new rspack.CopyRspackPlugin({
+            patterns: [
+                { from: './public/assets', to: 'assets' },
+            ],
+        }),
+        process.env.RSDOCTOR === 'true' &&
+        new RsdoctorRspackPlugin({
+            analyzerMode: 'server',
+            openAnalyzer: true,
+            port: 8888,
+        }),
+
+    ].filter(Boolean), // Filter out false values (e.g., if shouldAnalyze is false)
 });
